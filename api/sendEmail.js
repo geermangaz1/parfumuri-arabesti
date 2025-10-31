473f1f0f3d59e9adb1156254b1c3c32633b0d42d
geermangaz1 <88405469+geermangaz1@users.noreply.github.com>
2025-10-31 04:22:56 +0200

    Update sendEmail.js

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -8,7 +8,6 @@ export default async function handler(req, res) {
   try {
     const { name, email, phone, address } = req.body;
 
-    // Trimite emailul cu EmailJS
     const result = await emailjs.send(
       "service_db61zao", // Service ID
       "template_a68nvl9", // Template ID
@@ -18,12 +17,12 @@ export default async function handler(req, res) {
         phone,
         address,
       },
-      "Q49xH-BsQuOIHaXEy" // Public key
+      "Q49xH-BsQuOIHaXEy" // Public Key
     );
 
     res.status(200).json({ success: true, result });
   } catch (err) {
-    console.error("Eroare la trimitere:", err);
+    console.error("Eroare EmailJS:", err);
     res.status(500).json({ success: false, error: err.message });
   }
 }
